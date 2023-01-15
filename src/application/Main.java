c184e3ebc1ce1fa43b2392891117eb100f5b189d
Leonardo Santos <leosantos.vinte@gmail.com>
2023-01-15 15:49:04 -0300

    MainView

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,19 +9,21 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	@Override
-	public void start(Stage stage) {
-		try {
-			Parent parent = FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
-			Scene scene = new Scene(parent);
-			stage.setScene(scene);
-			stage.show();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void main(String[] args) {
+	@Override 
+	public void start(Stage primaryStage) { 
+	  try { 
+	    FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml")); 
+	    Parent parent = loader.load(); 
+	 
+	    Scene mainScene = new Scene(parent); 
+	    primaryStage.setScene(mainScene); 
+	    primaryStage.setTitle("Sample JavaFX application"); 
+	    primaryStage.show(); 
+	  } catch (IOException e) { 
+	    e.printStackTrace(); 
+	  } 
+	} 
+	 public static void main(String[] args) {
 		launch(args);
 	}
 }
